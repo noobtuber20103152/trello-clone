cd4d282264398f8ca5178e6fbe3190c3249157f9
ujjawal <noobtuber20103152>
2023-06-18 11:16:46 +0530

    yash project

--- component/navbar.tsx
+++ component/navbar.tsx
@@ -7,7 +7,7 @@ function Navbar(props:any) {
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
   <a href="https://flowbite.com/" className="flex items-center">
       <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo"/>
-      <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+      <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">toddle</span>
   </a>
   <div className="flex md:order-2">
       <button onClick={props.addList} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"> Create new board</button>
